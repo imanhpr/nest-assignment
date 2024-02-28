5242f0100fc0e8c153fbca3025474d1f99351c64
imanhpr <imanhpr1999@gmail.com>
2024-02-28 23:29:48 +0330

    feat: :safety_vest: Register global validation pipe for all DTOs

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,17 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
-
+import { ValidationPipe } from "@nestjs/common";
 import AppModule from "./app.module.js";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidUnknownValues: true })
   );
   await app.listen(3000);
 }
