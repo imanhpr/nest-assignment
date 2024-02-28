bc2e2135366d92900626563fd846174f86ef3991
imanhpr <imanhpr1999@gmail.com>
2024-02-28 23:22:58 +0330

    feat: :wrench: Add config for postgres database with mikro-orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,43 @@
-import { Module } from "@nestjs/common";
+import { Logger, Module, ValueProvider } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module.js";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+import { PostgreSqlDriver } from "@mikro-orm/postgresql";
+import { IEnvironmentVariables } from "./types/index.js";
 
+const MikroOrmQueryLogger: ValueProvider = {
+  provide: "MikroOrmQueryLogger",
+  useValue: new Logger("mikro-orm"),
+};
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({ cache: true })],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({ cache: true, isGlobal: true }),
+    MikroOrmModule.forRootAsync({
+      providers: [MikroOrmQueryLogger],
+      inject: [ConfigService, MikroOrmQueryLogger.provide],
+      useFactory(
+        configService: ConfigService<IEnvironmentVariables, false>,
+        logger: Logger
+      ) {
+        const DB_PASSWORD = configService.getOrThrow<string>("DB_PASSWORD");
+        const DB_USERNAME = configService.getOrThrow<string>("DB_USERNAME");
+        const DB_HOST = configService.getOrThrow<string>("DB_HOST");
+        const DB_NAME = configService.getOrThrow<string>("DB_NAME");
+        const clientUrl = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}`;
+        return {
+          entities: ["./dist/models"],
+          entitiesTs: ["./src/models"],
+          dbName: DB_NAME,
+          driver: PostgreSqlDriver,
+          clientUrl,
+          debug: true,
+          ensureDatabase: { forceCheck: true, create: true, clear: true },
+          forceUtcTimezone: true,
+          logger: (msg) => logger.debug(msg),
+        };
+      },
+    }),
+  ],
 })
 export default class AppModule {}
