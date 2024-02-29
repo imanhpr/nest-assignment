fedc9f8dcaeae48261943430a89db746e8557167
imanhpr <imanhpr1999@gmail.com>
2024-03-01 00:24:17 +0330

    refactor: :recycle: Update User model to add relationship with transaction and add currentBalance

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,4 +1,12 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import {
+  Collection,
+  Entity,
+  OneToMany,
+  PrimaryKey,
+  Property,
+} from "@mikro-orm/core";
+import Transaction from "./transaction.model.js";
+import { Decimal } from "decimal.js";
 
 @Entity({ tableName: "users" })
 export default class User {
@@ -10,4 +18,14 @@ export default class User {
 
   @Property({ fieldName: "password", nullable: false })
   password!: string;
+
+  @OneToMany(() => Transaction, (o) => o.user)
+  transactions = new Collection<Transaction>(this);
+
+  @Property({
+    nullable: false,
+    columnType: "NUMERIC(6,2)",
+    type: () => Decimal,
+  })
+  currentBalance: Decimal = new Decimal(0);
 }
