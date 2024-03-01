b573380726802efa7c707e5081cb77654240c257
imanhpr <imanhpr1999@gmail.com>
2024-03-01 18:05:14 +0330

    test: :white_check_mark: Add test for insertPrice fo currency service

--- src/currency/currency.service.spec.ts
+++ src/currency/currency.service.spec.ts
@@ -1,11 +1,24 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { CurrencyService } from "./currency.service";
 import { describe, beforeEach, it, expect } from "vitest";
-describe.todo("CurrencyService", () => {
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+import { SqliteDriver } from "@mikro-orm/sqlite";
+import Currency from "../models/currency.model.js";
+import Decimal from "decimal.js";
+describe("CurrencyService", () => {
   let service: CurrencyService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MikroOrmModule.forRoot({
+          allowGlobalContext: true,
+          driver: SqliteDriver,
+          dbName: ":memory:",
+          ensureDatabase: { create: true },
+          entities: [Currency],
+        }),
+      ],
       providers: [CurrencyService],
     }).compile();
 
@@ -14,5 +27,15 @@ describe.todo("CurrencyService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("should insert a record with name USD and value must be instaceof Decimal('56000')", async () => {
+    const data = { name: "USD", value: "56000" };
+    expect(service.insertPrice(data)).resolves.toEqual({
+      createdAt: expect.any(Date),
+      id: 1,
+      name: "USD",
+      value: new Decimal("56000"),
+    });
   });
 });
